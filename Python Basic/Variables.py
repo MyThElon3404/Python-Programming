3f0d830e0fc558394805f36854b2a22026d6d9f4
Manoj Pol <76487004+MyThElon3404@users.noreply.github.com>
2023-10-16 18:50:37 +0530

    Update Variables.py

--- Python Basic/Variables.py
+++ Python Basic/Variables.py
@@ -2,7 +2,7 @@
 # 1. A variable name must start with a letter or the underscore character
 # 2. A variable name cannot start with a number
 # 3. A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
-# 4. Variable names are case-sensitive (age, Age and AGE are three different variables)
+# 4. Variable names are case-sensitive (age, Age, and AGE are three different variables)
 # 5. A variable name cannot be any of the Python keywords.
 
 # Creating Variables - Python has no command for declaring a variable.
